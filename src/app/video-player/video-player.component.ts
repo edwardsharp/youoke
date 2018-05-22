089d65e31a254fff239f71c06dff40ba32e99127
edward sharp <edward@edwardsharp.net>
2018-05-22 18:24:15 -0400

    working yt player stubb :metal:

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 import { PlayerService } from '../player/player.service';
@@ -11,11 +11,8 @@ import { PlayerService } from '../player/player.service';
   src=""
   poster="">
 </video>
-<iframe 
-  #ytPlayer
-  src=""
-  frameborder="0" allowfullscreen></iframe>
-  
+<div id="player"></div>
+
 `, styles: [
   ':host{background-color:black; height: 100vh; overflow: hidden;}',
   'video, iframe{width:100%; height:100%; background-color:black;}'
@@ -23,7 +20,13 @@ import { PlayerService } from '../player/player.service';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  @ViewChild('ytPlayer') ytPlayer: any;
+  // @ViewChild('ytPlayer') ytPlayer: any;
+  // @ViewChild('ytPlayer') ytPlayer: any;
+ 
+  playerReady: boolean;
+  player: any;
+  donePlaying: boolean;
+
   needsVideoPlayer: boolean;
   needsYtPlayer: boolean;
   ytId: string;
@@ -36,16 +39,34 @@ export class VideoPlayerComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.playerService.getRows().then(rows => {
-      this.rowsChanged(rows);
-    });
+    
 
-    this.playerService.playerChange.subscribe(change => {
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit!!');
+    var tag = document.createElement('script');
+
+    //<script src="https://www.youtube.com/iframe_api"></script>
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window["onYouTubeIframeAPIReady"] = () => {
+      console.log("onYouTubeIframeAPIReady!");
+      this.playerReady = true;
+
       this.playerService.getRows().then(rows => {
         this.rowsChanged(rows);
-      }) 
-    });
+      });
 
+      this.playerService.playerChange.subscribe(change => {
+        this.playerService.getRows().then(rows => {
+          this.rowsChanged(rows);
+        }) 
+      });
+
+    }
+    
   }
 
   rowsChanged(rows:Array<any>){
@@ -53,12 +74,59 @@ export class VideoPlayerComponent implements OnInit {
     if(rows[0] && rows[0].name){
       this.ytId = rows[0].name;
       // this.ytSrc = `https://www.youtube.com/embed/${this.ytId}?rel=0&showinfo=0`;
-      this.needsYtPlayer = true;
-      this.ytPlayer.nativeElement.src = `https://www.youtube.com/embed/${this.ytId}?rel=0&showinfo=0`;
+      //this.needsYtPlayer = true;
+      //this.ytPlayer.nativeElement.src = `https://www.youtube.com/embed/${this.ytId}?rel=0&showinfo=0`;
       //this.sanitizer.bypassSecurityTrustResourceUrl();
       console.log('setting up player src:',`https://www.youtube.com/embed/${this.ytId}?rel=0&showinfo=0`);
+      
+      this.loadYtPlayer(rows[0].name);
+
       // console.log('sani fresh??',this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.ytId}?rel=0&showinfo=0`));
     }
+  }
+
+  loadYtPlayer(vID: string){
+
+    this.player = new window["YT"].Player('player', {
+      // enablejsapi: 1,
+      // origin: window.location.origin,
+      // host: 'https://www.youtube.com',
+      playerVars: {
+        modestbranding: 1,
+        showinfo: 0,
+        controls: 0,
+        disablekb: 1,
+        rel: 0
+      },
+      height: '100%',
+      width: '100%',
+      videoId: vID,
+      events: {
+        'onReady': (event) => {
+          console.log('onReady! event:',event);
+          event.target.playVideo();
+        },
+        'onStateChange': (event) => {
+          console.log('onStateChange event:',event);
+          if (event.data == window["YT"].PlayerState.PLAYING && !this.donePlaying) {
+            setTimeout(() => {
+              this.player.stopVideo();
+            }, 6000);
+            this.donePlaying = true;
+          }
+        }
+      }
+    });
+
+    // 4. The API will call this function when the video player is ready.
+    
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    
+    
+    
   }
 
 }
